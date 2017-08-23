94f6e6368f748f5076732a133a6974c014792136
ofnerj <30871694+ofnerj@users.noreply.github.com>
2017-08-23 15:02:47 -0400

    WWW Final Project
    WWW Final Project

--- WWW Project/login.php
+++ WWW Project/login.php
@@ -1,6 +1,6 @@
 <?php // login.php
     $hn = 'localhost'; //hostname
-    $db = 'ofnerj_project3'; //database
-    $un = 'ofnerj_project3'; //username
+    $db = 'ofnerj_project'; //database
+    $un = 'ofnerj_project'; //username
 	$pw = 'test123'; //password
 ?>
